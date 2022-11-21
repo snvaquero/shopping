771e9b89b06cdaecec7bd30dddf7b94ad3ebd6f1
Fazt <contact@fazweb.com>
2022-11-21 13:22:26 -0600

    EntitiFrameWorkCore CodeFirst

---
+++ Shopping/Shopping/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options ):base(options)
+        {
+
+        }
+        public DbSet<Country> Countries  { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display(Name="Pais")]
+        [MaxLength(50,ErrorMessage ="El campo {0} debe tener maximo {1} caracteres")]
+        [Required (ErrorMessage ="El campo {0} es obligatorio ")]
+        public string Name { get; set; }
+    }
+}
