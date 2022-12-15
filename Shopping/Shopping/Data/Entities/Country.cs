3cc3d9b802be710abdaca2f66fb2f41e1311c3de
Fazt <contact@fazweb.com>
2022-12-14 22:50:24 -0600

    Begin with state CRUD

--- Shopping/Shopping/Data/Entities/Country.cs
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -9,5 +9,10 @@ namespace Shopping.Data.Entities
         [MaxLength(50,ErrorMessage ="El campo {0} debe tener maximo {1} caracteres")]
         [Required (ErrorMessage ="El campo {0} es obligatorio ")]
         public string Name { get; set; }
+
+        public ICollection<State> States { get; set; }
+        [Display(Name = "Departamentos/Estados")]
+
+        public int StatesNumber => States == null ? 0 : States.Count;
     }
 }
---
+++ Shopping/Shopping/Models/StateViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Models
+{
+    public class StateViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Departamento/Estado")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio ")]
+        public string Name { get; set; }
+
+        public int CountryId { get; set; }
+    }
+}
